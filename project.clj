2308eacfadb820e9f8c125862bf75b148b04b353
CI/CD <ci@pinkgorilla.org>
2020-04-28 21:01:09 +0000

    Release 0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/clojisr-gorilla "0.0.1-SNAPSHOT"
+(defproject org.pinkgorilla/clojisr-gorilla "0.0.1"
   :description "Clojure<->R interop"
   :license {:name "MIT"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
